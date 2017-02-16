fa60ddd9c1677cebee6d54c32522b9607059a957
Martin Lindhe <martin-commit@ubique.se>
2017-02-16 23:25:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# About
+
+XXX
+
+
+# XXX
+
+lists all files in input directory
+give flags to filter output
+    --min-size = X
+        OR
+    --max-size = X
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var (
+	inDir   = kingpin.Arg("inDir", "Input directory.").String()
+	minSize = kingpin.Flag("min-size", "Minimum size in bytes.").Int64()
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+	kingpin.Parse()
+}
+
+func main() {
+	if *inDir == "" {
+		// log.Println("DEBUG: No indir provided, using current")
+		*inDir = "./"
+	}
+
+	finder, err := NewFileFinder(*inDir)
+	finder.minSize = *minSize
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	finder.SearchAndPrint()
+}
+
+type FileFinder struct {
+	rootDir string
+	minSize int64
+}
+
+func NewFileFinder(inDir string) (*FileFinder, error) {
+	if _, err := os.Stat(inDir); err != nil && os.IsNotExist(err) {
+		return nil, err
+	}
+
+	rootDir, err := filepath.Abs(inDir)
+	if err != nil {
+		return nil, err
+	}
+
+	find := FileFinder{
+		rootDir: rootDir,
+	}
+	return &find, nil
+}
+
+func (find *FileFinder) SearchAndPrint() {
+
+	log.Println("Searching in", find.rootDir)
+
+	filepath.Walk(find.rootDir, func(fp string, fi os.FileInfo, err error) error {
+		if err != nil {
+			log.Println(err) // can't walk here,
+			return nil       // but continue walking elsewhere
+		}
+		if fi.IsDir() {
+			return nil // not a file.  ignore.
+		}
+		matched, err := filepath.Match("*", fi.Name()) // XXX "*.mp3" to match only this extension
+		if err != nil {
+			log.Println(err) // malformed pattern
+			return err       // this is fatal.
+		}
+
+		// check additional criteria
+		if find.minSize != 0 {
+			if fi.Size() < find.minSize {
+				log.Println("DEBUG: file too small so hiding", fi.Name())
+				matched = false
+			}
+		}
+
+		if matched {
+			fmt.Println(fp)
+		}
+		return nil
+	})
+}
+
+// exists reports whether the named file or directory exists.
+func exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
